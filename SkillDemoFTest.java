673594a9db0537b585b499baace378411f93c63a
vickyc0221 <vcaldero@ucsd.edu>
2022-02-04 14:32:22 -0800

    Update SkillDemoFTest.java

--- SkillDemoFTest.java
+++ SkillDemoFTest.java
@@ -1,8 +1,7 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class SkillDemoFTest {
-    SkillDemoF myDemo = new SkillDemoF();
     @Test
 	public void addition() {
 		assertEquals(2, SkillDemoF.add(1, 3));
