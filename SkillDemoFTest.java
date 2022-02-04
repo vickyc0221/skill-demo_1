6f28ac7399c68724c4d7d7f218673230444bba75
vickyc0221 <vcaldero@ucsd.edu>
2022-02-04 14:24:31 -0800

    Update SkillDemoFTest.java

--- SkillDemoFTest.java
+++ SkillDemoFTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillDemoFTest {
+    SkillDemoF myDemo = new SkillDemoF();
     @Test
 	public void addition() {
 		assertEquals(2, SkillDemoF.add(1, 3));
